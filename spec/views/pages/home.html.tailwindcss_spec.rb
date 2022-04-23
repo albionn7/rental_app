b6834a03126090046223d63880ff981d2e649460
albionn7 <albionnazari@hotmail.com>
2022-04-23 10:59:48 +0200

    added nav bar and specs for homepage

--- spec/views/pages/home.html.tailwindcss_spec.rb
+++ spec/views/pages/home.html.tailwindcss_spec.rb
@@ -1,7 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe "pages/home.html.tailwindcss", type: :view do
-    it 'renders the home pages' do
+    it 'displays the nav bar' do
+      render partial: 'shared/navbar'
 
+      expect(rendered).to include('All listings', 'Buy', 'Rent', 'Sell')
     end
 end
